80124f9e4ed2a396bd8ad245f81dd0422a54987c
Ada Kao <adakao@zdev.local>
2018-04-25 14:44:59 +0800

    first commit

---
+++ WebServiceSDK/ASWebServiceSDK.h
@@ -0,0 +1,13 @@
+//
+//  ASWebServiceSDK.h
+//  WebServiceSDK
+//
+//  Created by Ada Kao on 25/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASWebServiceSDK : NSObject
+
+@end
---
+++ WebServiceSDK/ASWebServiceSDK.m
@@ -0,0 +1,13 @@
+//
+//  ASWebServiceSDK.m
+//  WebServiceSDK
+//
+//  Created by Ada Kao on 25/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import "ASWebServiceSDK.h"
+
+@implementation ASWebServiceSDK
+
+@end
