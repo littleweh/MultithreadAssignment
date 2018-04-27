8b255522708d2b5c8622ce40d306ead451dcb416
littleweh <adakao@gmail.com>
2018-04-27 10:11:02 +0800

    fix POSTmethod

--- WebServiceSDK/ASWebServiceSDK.m
+++ WebServiceSDK/ASWebServiceSDK.m
@@ -8,6 +8,8 @@
 
 #import "ASWebServiceSDK.h"
 
+const static NSString * ASTTTT  = @"xxxxx";
+
 @interface ASWebServiceSDK ()
 @property (strong, nonatomic, readwrite) NSString *httpbinDomain;
 @property (strong, nonatomic, readwrite) NSString *endPointGet;
@@ -67,18 +69,27 @@
                                                 } else {
                                                     callback(nil, parseJsonError);
                                                 }
+//                                                NSMutableDictionary *params = [[NSMutableDictionary alloc] initWithDictionary:@{}];
+//                                                params = @{@"abc": data[@"sfs"] ?: @""};
+                                                // Mantle
     }];
     [dataTask resume];
 }
 
 -(void)postCustomerName:(NSString *)name callback: (void(^)(NSDictionary *, NSError *)) callback {
-    NSString *postURLString = [[NSString alloc] initWithFormat:@"%@%@?custname=%@", self.httpbinDomain, self.endPointPost, name];
+    NSString *postURLString = [[NSString alloc] initWithFormat:@"%@%@", self.httpbinDomain, self.endPointPost];
     NSURL *postURL = [NSURL URLWithString:postURLString];
+
+    NSString *post = [NSString stringWithFormat:@"custname=%@", name];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding];
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
     
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:postURL
                                                            cachePolicy:NSURLRequestUseProtocolCachePolicy
                                                        timeoutInterval:10.0];
     [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody:postData];
     
     NSURLSession *session = [NSURLSession sharedSession];
     NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
@@ -87,7 +98,9 @@
                                                                     NSError * _Nullable error) {
                                                     // response check? how?
                                                     if ([response respondsToSelector:@selector(statusCode)]) {
-                                                        if ([(NSHTTPURLResponse *) response statusCode] == 404) {
+                                                        if ([(NSHTTPURLResponse *) response statusCode] == 500) {
+                                                            return;
+                                                        } else if ([(NSHTTPURLResponse *) response statusCode] == 404) {
                                                             callback(nil, error);
                                                         }
                                                     }
