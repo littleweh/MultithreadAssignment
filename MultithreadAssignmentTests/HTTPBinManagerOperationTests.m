bd78a1dad55110919f3a2d1dfee988db1f77f39a
Ada Kao <adakao@zdev.local>
2018-05-02 18:29:41 +0800

    try to call HTTPBinManager executeOperation, but there is a bug; operation's delegate (manager) is dead after fetchGet and postcustname method

---
+++ MultithreadAssignmentTests/HTTPBinManagerOperationTests.m
@@ -0,0 +1,29 @@
+//
+//  HTTPBinManagerOperationTests.m
+//  MultithreadAssignmentTests
+//
+//  Created by Ada Kao on 02/05/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HTTPBinManagerOperationTests : XCTestCase
+
+@end
+
+@implementation HTTPBinManagerOperationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+@end
