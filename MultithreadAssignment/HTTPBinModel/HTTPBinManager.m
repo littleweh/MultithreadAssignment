7f1fed5d75c34fe3c1ed34ccfbe78366b3de5eac
Ada Kao <adakao@zdev.local>
2018-05-03 09:55:20 +0800

    delete some NSLog

--- MultithreadAssignment/HTTPBinModel/HTTPBinManager.m
+++ MultithreadAssignment/HTTPBinModel/HTTPBinManager.m
@@ -34,6 +34,7 @@
 }
 
 -(void) executeOperation: (HTTPBinManagerOperation *) operation {
+    NSAssert(operation != nil && [operation isKindOfClass:[HTTPBinManagerOperation class]], @"operation should be HTTPBinManagerOperation and not nil");
     [operation setDelegate:self];
     [self.operationQueue cancelAllOperations];
     [self.operationQueue addOperation:operation];
@@ -42,21 +43,18 @@
 
 #pragma mark: HTTPBinManagerOperationDelegate
 - (void)httpBinManagerOperation:(HTTPBinManagerOperation *)operation didGetObject:(NSArray<NSDictionary *> *)objects didGetImage:(UIImage *)image {
-    NSLog(@"in HTTPBinManager, jsonObjects: %@", objects);
     if ([self.delegate respondsToSelector:@selector(httpBinManager:didGetObject:didGetImage:)]) {
         [self.delegate httpBinManager:self didGetObject:objects didGetImage:image];
     }
 }
 
 - (void)httpBinManagerOperation:(HTTPBinManagerOperation *)operation progress:(CGFloat)progressPercentage {
-    NSLog(@"in HTTPBinManager, progressPercentage: %f", progressPercentage);
     if ([self.delegate respondsToSelector:@selector(httpBinManager:progress:)]) {
         [self.delegate httpBinManager:self progress:progressPercentage];
     }
 }
 
 - (void)httpBinManagerOperation:(HTTPBinManagerOperation *)operation status:(HTTPBinManagerOperationStatus)statusCode {
-    NSLog(@"in HTTPBinManager, status code: %ld", (unsigned long)statusCode);
     if ([self.delegate respondsToSelector:@selector(httpBinManager:status:)]) {
         [self.delegate httpBinManager:self status:statusCode];
     }
